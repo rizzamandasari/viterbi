7e27f1a9ffe7dd9fd476822f70cc1bb0f0cd17c9
ijonk <valgard.aegist@gmail.com>
2011-06-23 21:57:22 +0700

    firs

--- src/edu/rizza/ihmm/DNA.java
+++ src/edu/rizza/ihmm/DNA.java
@@ -1,10 +1,9 @@
-package edu.rizza.ihmm;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DNA {
@@ -12,15 +11,17 @@ public class DNA {
 	private String sequence;
 	private char[] sequenceArray;
 	private String filename;
-	private List<Gen> genes = null;// ada lampu pke tnd silang
+	private final String genFilename;
+	private List<Gen> genes = new ArrayList<Gen>();
 
-	// private List genes = new ArrayList(); // ada lampu pke tnd silang
+	public DNA(String filename, String genFilename) {
+		this.filename = filename;
+		this.genFilename = genFilename;
+		preprocessing();
 
-	public DNA(String filename) {
-		preprocessing(filename);
 	}
 
-	private void preprocessing(String filename) {
+	private void preprocessing() {
 		FileInputStream finput = null;
 
 		// membuka file
@@ -47,6 +48,9 @@ public class DNA {
 			System.out.println("Error IO");
 			return;
 		}
+	}
+
+	public void loadGenes(String filename) {
 
 	}
 
@@ -79,41 +83,76 @@ public class DNA {
 	}
 
 	public List<Gen> getGenes() {
-		if (genes != null) {
+		if (genes.size() != 0) {
 			return genes;
 		}
-		StringBuilder sb = null;
+		FileInputStream finput = null;
+		try {
+			finput = new FileInputStream(genFilename);
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(finput));
+			String line = null;
 
-		for (int i = 0; i < (sequenceArray.length - 2); i++) {
-			sb = new StringBuilder(sequenceArray[i]);
-			String str = sb.append(sequenceArray[i + 1]).append(i + 2)
-					.toString();
-			// cari startcodon
-			if (str.equalsIgnoreCase("atg") || str.equalsIgnoreCase("gtg")) {
-				Integer[] startCodon = { new Integer(i), new Integer(i + 1),
-						new Integer(i + 2) };
-				genes.add(new Gen(startCodon));
+			// ini mw gw ganti, jadi buat ngsearch atg
+			//
+
+			while ((line = br.readLine()) != null) {
+				Gen gen = new Gen();
+				String[] str = line.split("-", 2);
+				int startCodonIndex = Integer.parseInt(str[0]);
+				int stopCodonIndex = Integer.parseInt(str[1]);
+				if (stopCodonIndex > startCodonIndex) {
+					Integer[] startCodon = { new Integer(startCodonIndex),
+							new Integer(startCodonIndex + 1),
+							new Integer(startCodonIndex + 2) };
+					gen.setStartCodon(startCodon);
+					Integer[] stopCodon = { new Integer(stopCodonIndex - 2),
+							new Integer(stopCodonIndex - 1),
+							new Integer(stopCodonIndex) };
+					gen.setStopCodon(stopCodon);
+					String[] codingRegion = new String[700000];
+					int j = 0;
+					for (int i = startCodonIndex + 3; i < stopCodonIndex - 2; i = i + 3) {
+						codingRegion[j] = sequence.substring(i, i + 2);
+						j++;
+					}
+					// System.out.println(startCodonIndex);
+					gen.setCodingRegion(codingRegion);
+					gen.setSequence(sequence.substring(startCodonIndex,
+							stopCodonIndex));
+
+				} else {
+
+					Integer[] startCodon = { new Integer(startCodonIndex),
+							new Integer(startCodonIndex + 1),
+							new Integer(startCodonIndex + 2) };
+					gen.setStartCodon(startCodon);
+					Integer[] stopCodon = { new Integer(stopCodonIndex - 2),
+							new Integer(stopCodonIndex - 1),
+							new Integer(stopCodonIndex) };
+					gen.setStopCodon(stopCodon);
+					gen.setSequence(sequence.substring(stopCodonIndex,
+							startCodonIndex));
+				}
+				genes.add(gen);
 			}
-
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		return genes;
 	}
-
-	public void stopCodon() {
-		StringBuilder sb = null;
-		for (int i = 0; i < (sequenceArray.length - 2); i++) {
-			sb = new StringBuilder(sequenceArray[i]);
-			String str = sb.append(sequenceArray[i + 1]).append(i + 2)
-					.toString();
-			if (str.equalsIgnoreCase("tag") || str.equalsIgnoreCase("tga")
-					|| str.equalsIgnoreCase("taa")) {
-				Integer[] startCodon = { new Integer(i), new Integer(i + 1),
-						new Integer(i + 2) };
-				genes.add(new Gen(stopCodon)); // error dsni
-			}
-			return;
-		}
-	}
-
+	/*
+	 * public void stopCodon() { StringBuilder sb = null; for (int i = 0; i <
+	 * (sequenceArray.length - 2); i++) { sb = new
+	 * StringBuilder(sequenceArray[i]); String str = sb.append(sequenceArray[i +
+	 * 1]).append(i + 2) .toString(); if (str.equalsIgnoreCase("tag") ||
+	 * str.equalsIgnoreCase("tga") || str.equalsIgnoreCase("taa")) { Integer[]
+	 * startCodon = { new Integer(i), new Integer(i + 1), new Integer(i + 2) };
+	 * genes.add(new Gen(stopCodon)); // error dsni } return; }
+	 */
 }
--- src/edu/rizza/ihmm/Gen.java
+++ src/edu/rizza/ihmm/Gen.java
@@ -4,14 +4,85 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Gen {
-	private String[] startCodon;
-	private String[] stopCodon;
+	private Integer[] startCodon;
+	private Integer[] stopCodon;
 	private String[] codingRegion;
+	private String sequence;
+	private char[] basaStartCodon;
+	private char[] basaCodingRegion;
+	private char[] basaStopCodon;
 
-	private final List<Gen> genes = new ArrayList<Gen>();
+	private final List<Double> alphaTypical1 = new ArrayList<Double>();
+	private final List<Double> alphaTypical2 = new ArrayList<Double>();
+	private final List<Double> alphaTypical3 = new ArrayList<Double>();
 
-	public Gen(String[] startCodon) {
-		this.startCodon = startCodon;
+	private final List<Double> betaTypical1 = new ArrayList<Double>();
+	private final List<Double> betaTypical2 = new ArrayList<Double>();
+	private final List<Double> betaTypical3 = new ArrayList<Double>();
+
+	private final List<Double> alphaATypical1 = new ArrayList<Double>();
+	private final List<Double> alphaATypical2 = new ArrayList<Double>();
+	private final List<Double> alphaATypical3 = new ArrayList<Double>();
+
+	private final List<Double> betaATypical1 = new ArrayList<Double>();
+	private final List<Double> betaATypical2 = new ArrayList<Double>();
+	private final List<Double> betaATypical3 = new ArrayList<Double>();
+
+	/*
+	 * public void isComplete() { Iterator<Gen> iterator = genes.iterator();
+	 * while (iterator.hasNext()) { if(genes.equals(stopCodon)){
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
+
+	public List<Double> getAlphaTypical1() {
+		return alphaTypical1;
+	}
+
+	public List<Double> getAlphaTypical2() {
+		return alphaTypical2;
+	}
+
+	public List<Double> getAlphaTypical3() {
+		return alphaTypical3;
+	}
+
+	public List<Double> getBetaTypical1() {
+		return betaTypical1;
+	}
+
+	public List<Double> getBetaTypical2() {
+		return betaTypical2;
+	}
+
+	public List<Double> getBetaTypical3() {
+		return betaTypical3;
+	}
+
+	public List<Double> getAlphaATypical1() {
+		return alphaATypical1;
+	}
+
+	public List<Double> getAlphaATypical2() {
+		return alphaATypical2;
+	}
+
+	public List<Double> getAlphaATypical3() {
+		return alphaATypical3;
+	}
+
+	public List<Double> getBetaATypical1() {
+		return betaATypical1;
+	}
+
+	public List<Double> getBetaATypical2() {
+		return betaATypical2;
+	}
+
+	public List<Double> getBetaATypical3() {
+		return betaATypical3;
 	}
 
 	public boolean isCompete() {
@@ -19,19 +90,19 @@ public class Gen {
 	}
 
 	// geter setter
-	public String[] getStartCodon() {
+	public Integer[] getStartCodon() {
 		return startCodon;
 	}
 
-	public void setStartCodon(String[] startCodon) {
+	public void setStartCodon(Integer[] startCodon) {
 		this.startCodon = startCodon;
 	}
 
-	public String[] getStopCodon() {
+	public Integer[] getStopCodon() {
 		return stopCodon;
 	}
 
-	public void setStopCodon(String[] stopCodon) {
+	public void setStopCodon(Integer[] stopCodon) {
 		this.stopCodon = stopCodon;
 	}
 
@@ -43,4 +114,120 @@ public class Gen {
 		this.codingRegion = codingRegion;
 	}
 
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+
+	public String getSequence() {
+		return sequence;
+	}
+
+	public void setBasaStartCodon(char[] basaStartCodon) {
+		this.basaStartCodon = basaStartCodon;
+	}
+
+	public char[] getBasaStartCodon() {
+		if (basaStartCodon == null) {
+			basaStartCodon = sequence.substring(0, 3).toCharArray();
+		}
+		return basaStartCodon;
+	}
+
+	public void setBasaCodingRegion(char[] basaCodingRegion) {
+		this.basaCodingRegion = basaCodingRegion;
+	}
+
+	public char[] getBasaCodingRegion() {
+		if (basaCodingRegion == null) {
+			basaCodingRegion = sequence.substring(3, (sequence.length() - 3))
+					.toCharArray();
+		}
+		return basaCodingRegion;
+	}
+
+	public void setBasaStopCodon(char[] basaStopCodon) {
+		this.basaStopCodon = basaStopCodon;
+	}
+
+	public boolean isTypical() {
+		return sequence.length() <= 1506;
+
+	}
+
+	public char[] getBasaStopCodon() {
+		if (basaStopCodon == null) {
+			basaStopCodon = sequence.substring((sequence.length() - 3),
+					(sequence.length())).toCharArray();
+		}
+		return basaStopCodon;
+	}
+
+	public double sumStateTypical(int state) {
+		double sumState = 0;
+
+		int index = 0;
+		for (double alpha : alphaTypical2) {
+			if (index % 3 == state) {
+				sumState += alpha
+						* betaTypical2.get(alphaTypical2.size() - index - 1);
+				// out of bound
+			}
+			index++;
+		}
+		return sumState;
+	}
+
+	public double sumStateATypical(int state) {
+		double sumState = 0;
+
+		int index = 0;
+
+		for (double alpha : alphaATypical2) {
+			if (index % 3 == state) {
+				sumState += alpha
+						* betaATypical2.get(alphaATypical2.size() - index - 1);
+			}
+			index++;
+		}
+		return sumState;
+
+	}
+
+	public double sumCharTypical(int state, char c) {
+
+		double sumChar = 0;
+
+		int index = 0;
+		for (double alpha : alphaTypical2) {
+			if (index % 3 == state && basaCodingRegion[index] == c) {
+				sumChar += alpha
+						* betaTypical2.get(alphaTypical2.size() - 1 - index);
+			}
+			index++;
+		}
+		return sumChar;
+
+	}
+
+	public double sumCharATypical(int state, char c) {
+		double sumChar = 0;
+
+		int index = 0;
+		for (double alpha : alphaTypical2) {
+			if (index % 3 == state && basaCodingRegion[index] == c) {
+				sumChar += alpha
+						* betaTypical2.get(alphaTypical2.size() - 1 - index);
+			}
+			index++;
+		}
+		return sumChar;
+	}
+
+	public double sumStateStart() {
+
+		double sumState = 0;
+		sumState = basaStartCodon[0];
+
+		return sumState;
+	}
 }
